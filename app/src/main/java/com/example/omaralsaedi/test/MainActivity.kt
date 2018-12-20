09c3050097ee4e96394a3e3291ec81c91940d7ab
omar5552018 <omar5552016@hotmail.com>
2018-12-20 22:33:58 +0300

    add another Button

--- app/src/main/java/com/example/omaralsaedi/test/MainActivity.kt
+++ app/src/main/java/com/example/omaralsaedi/test/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val team = "omar, john, jamal, smith"
+        val omar = "omar"
     }
 }
